84e393cf190129777151e97ccb1a1ca7b2879d5b
KalyDimitrova <kal4omal4o@gmail.com>
2026-02-28 11:17:02 +0200

    created cells classes and interface, stored everything in packages

---
+++ src/bg/tu_varna/sit/f24621616/OOP1_project/cells/FormulaCell.java
@@ -0,0 +1,12 @@
+package bg.tu_varna.sit.f24621616.OOP1_project.cells;
+
+import bg.tu_varna.sit.f24621616.OOP1_project.interfaces.Cell;
+
+public class FormulaCell implements Cell {
+
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
+}
